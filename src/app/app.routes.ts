932b3943e021eb098f3fb454cc4da69c53958323
Subham Kumar Sathua <137478528+SubhamSathua@users.noreply.github.com>
2025-10-09 18:45:58 +0530

    Updated Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,23 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './login/login';
 import { BookRoom } from './book-room/book-room';
 import { About } from './about/about';
+import { NgModule } from '@angular/core';
+import { Register } from './register/register';
 
 export const routes: Routes = [
-    { path: '', component: Home},
-    { path: 'home', component: Home },
-    { path: 'book', component: BookRoom },
-    { path: 'about', component: About },
-    { path: 'login', component: Login }
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'book', component: BookRoom },
+  { path: 'about', component: About },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: '**', component: Error }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,49 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink, HttpClientModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
+  // login() {
+  //   throw new Error('Method not implemented.');
+  // }
+  // password: any;
+  // username: any;
+
+  username: string = '';
+  password: string = '';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login() {
+    const loginData = { username: this.username, password: this.password };
+
+    this.http.post('http://localhost:8080/api/login', loginData)
+      .subscribe(
+        (response: any) => {
+          console.log('Login response:', response);
+          if (response.role) {
+            if (response.role === 'ADMIN') {
+              this.router.navigate(['/admin-dashboard']);
+            } else if (response.role === 'USER') {
+              this.router.navigate(['/user-dashboard']);
+            }
+          } else {
+            alert('Invalid credentials');
+          }
+        },
+        (error) => {
+          console.error('Login error:', error);
+          alert('Server error or wrong credentials');
+        }
+      );
+  }
 
 }
